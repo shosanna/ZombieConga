ea53d09b09fd15c7c85a69ddd722ddccef2894da
Zuzana Dostalova <dostalovaz@gmail.com>
2015-03-18 22:21:49 +0100

    initial commit

--- ZombieConga/GameScene.swift
+++ ZombieConga/GameScene.swift
@@ -9,37 +9,56 @@
 import SpriteKit
 
 class GameScene: SKScene {
-    override func didMoveToView(view: SKView) {
-        /* Setup your scene here */
-        let myLabel = SKLabelNode(fontNamed:"Chalkduster")
-        myLabel.text = "Hello, World!";
-        myLabel.fontSize = 65;
-        myLabel.position = CGPoint(x:CGRectGetMidX(self.frame), y:CGRectGetMidY(self.frame));
-        
-        self.addChild(myLabel)
-    }
+    // creating zombie
+    var zombie = SKSpriteNode(imageNamed: "zombie1")
+    let zombieMovePointsPerSec: CGFloat = 480
+    var velocity = CGPointZero
     
-    override func touchesBegan(touches: NSSet, withEvent event: UIEvent) {
-        /* Called when a touch begins */
+    var lastUpdateTime: NSTimeInterval = 0
+    var dt: NSTimeInterval = 0
+    
+    override func didMoveToView(view: SKView) {
+        // adding background
+        let background = SKSpriteNode(imageNamed: "background1")
+        background.position = CGPoint(x: size.width/2, y: size.height/2)
         
-        for touch: AnyObject in touches {
-            let location = touch.locationInNode(self)
-            
-            let sprite = SKSpriteNode(imageNamed:"Spaceship")
-            
-            sprite.xScale = 0.5
-            sprite.yScale = 0.5
-            sprite.position = location
-            
-            let action = SKAction.rotateByAngle(CGFloat(M_PI), duration:1)
-            
-            sprite.runAction(SKAction.repeatActionForever(action))
-            
-            self.addChild(sprite)
-        }
+        // default is 0, so if it is negative number it always will be drawn first
+        background.zPosition = -1
+        
+        // every sprite needs to be added to the scene
+        addChild(background)
+        
+        // adding zombie
+        zombie.position = CGPoint(x: 400, y: 400)
+        zombie.name = "zombie"
+        addChild(zombie)
     }
    
     override func update(currentTime: CFTimeInterval) {
         /* Called before each frame is rendered */
+        
+        // Updating the delta
+        if lastUpdateTime > 0 {
+            dt = currentTime - lastUpdateTime
+        } else {
+            dt = 0
+        }
+        
+        lastUpdateTime = currentTime
+        
+        moveSprite(zombie, velocity: CGPoint(x: zombieMovePointsPerSec, y: 0))
+    }
+    
+    // This function calculates the right amount of steps for each frame
+    func moveSprite(sprite: SKSpriteNode, velocity: CGPoint) {
+        // velocity is a vector representing a direction and a length
+        let amountToMove = CGPoint(x: velocity.x * CGFloat(dt), y: velocity.y * CGFloat(dt))
+        
+        sprite.position = CGPoint(x: sprite.position.x + amountToMove.x, y: sprite.position.y + amountToMove.y)
+    }
+    
+    func moveZombieToward(location: CGPoint) {
+        let offset = CGPoint(x: location.x - zombie.position.x, y: location.y - zombie.position.y)
+        
     }
 }
